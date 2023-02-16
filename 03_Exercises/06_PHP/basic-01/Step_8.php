58348fae4efdf06fca3b16e84a3873e132dee686
mahdi halloum <mahdinah98@gmail.com>
2023-02-16 23:09:35 +0200

    step-8-a

---
+++ 03_Exercises/06_PHP/basic-01/Step_8.php
@@ -0,0 +1,10 @@
+<?php
+$items=array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+echo "Hello sir, do you have "
+    . $items[0] . ", "
+    . $items[1] . ", "
+    . "and "
+    . $items[2] . "? Also if you sell fruits can I find a "
+    . $items[5] . "?";
+
+?>
