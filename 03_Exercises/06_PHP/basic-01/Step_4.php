19d938e39dafcd87c17f5bb836fe4c91f989949d
mahdi halloum <mahdinah98@gmail.com>
2023-02-16 22:27:00 +0200

    step-4

---
+++ 03_Exercises/06_PHP/basic-01/Step_4.php
@@ -0,0 +1,12 @@
+<?php
+$name="mahdi";
+$position="full stack web developer";
+$github_url="https://github.com/mahdinah/Documentation/tree/master/03_Exercises/06_PHP";
+echo "Hello, I'm " . $name . ", I'm a " . $position . " please check my github link <br/>" . $github_url;
+echo "<br /><br />";
+echo "Hello, I'm $name, I'm a $position please check my github link <br/>$github_url";
+echo "<br /><br />";
+echo 'Hello, I\'m ' . $name . ', I\'m a ' . $position . ' please check my github link <br/>' . $github_url;
+echo "<br /><br />";
+echo 'Hello, I\'m ', $name, ', I\'m a ', $position, ' please check my github link <br/>', $github_url;
+?>
