1e7884258d6e7007084596781bf38d6f722c3fa5
mahdi halloum <mahdinah98@gmail.com>
2023-02-16 22:16:43 +0200

    step 4

---
+++ 03_Exercises/06_PHP/basic-01/Step_3.php
@@ -0,0 +1,10 @@
+
+<?php
+$name="Mahdi";
+$email="mahdinah98@gmail.com";
+echo"</br> My name is $name </br> My email is$email";
+$height=169;
+$width=70;
+$area=$height*$width;
+echo "</br>$area";
+?>
