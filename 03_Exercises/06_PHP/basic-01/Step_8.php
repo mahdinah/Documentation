ea096ca381b1ffca51a36351b3bd086129ee8813
mahdi halloum <mahdinah98@gmail.com>
2023-02-16 23:20:08 +0200

    step-8-b

--- 03_Exercises/06_PHP/basic-01/Step_8.php
+++ 03_Exercises/06_PHP/basic-01/Step_8.php
@@ -5,6 +5,18 @@ echo "Hello sir, do you have "
     . $items[1] . ", "
     . "and "
     . $items[2] . "? Also if you sell fruits can I find a "
-    . $items[5] . "?";
+    . $items[5] . "?</br>";
 
+?>
+<?php
+$grocery=array( "eggs" => array("balade", "mazere3"),
+                "milk" => array("Fresh", "Taanayel"),
+                "water-pack" => array("Tanoureen", "Reem"),
+);
+echo "Hey Sir, Please I need 1 pack of balade eggs and 3 Reem Water Pack";
+echo "<br>Hey Sir, Please I need 1 pack of "
+    . $grocery["eggs"][0]
+    . " eggs and 3 "
+    . $grocery["water-pack"][1]
+    . " Water Pack.";
 ?>
