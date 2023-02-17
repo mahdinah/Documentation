9e2a06233c9f2ca277ca6fd9a60a1a0d70e1e7ec
mahdi halloum <mahdinah98@gmail.com>
2023-02-17 11:05:19 +0200

    step-5

---
+++ 03_Exercises/06_PHP/basic-02/Step_5.php
@@ -0,0 +1,20 @@
+<?php
+$word ="six";
+    switch($word){
+    case "zero":
+        echo "0";
+    break;
+    case "one":
+        echo  "1";
+    break;
+    case "five":
+        echo  "5";
+    break;
+    case "three":
+        echo "3";
+    break;
+    default:
+    echo "word not found";
+}
+
+?>
