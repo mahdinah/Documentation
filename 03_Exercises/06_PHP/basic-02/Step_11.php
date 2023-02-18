d1778c471ccc65be846782c1bdbf4befcdc0f6c9
mahdi halloum <mahdinah98@gmail.com>
2023-02-18 17:10:13 +0200

    step-11

---
+++ 03_Exercises/06_PHP/basic-02/Step_11.php
@@ -0,0 +1,23 @@
+<?php
+function prime($number)
+{
+    if ($number == 1) {
+        return 0;
+    }
+    for ($i = 2; $i <= $number / 2; $i++) {
+        if ($number % $i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+$n = 23;
+$result = prime($n);
+if ($result == 1) {
+    echo "$n is a Prime";
+} else {
+    echo "$n is Not a Prime";
+}
+
+?>
